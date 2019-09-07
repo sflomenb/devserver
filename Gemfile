0b6b2a4fa64de8669a9b06aa7ce2bdc26d1616c2
Sam Flomenberg <sam.flomenberg@gmail.com>
2019-09-07 19:22:04 -0400

    feat: add dockerspec

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'dockerspec', '~> 0.5.0'
---
+++ spec/devserver_spec.rb
@@ -0,0 +1,67 @@
+# spec/myapp_spec.rb
+
+require 'dockerspec/serverspec'
+
+describe 'My Dockerfile' do
+  describe docker_build('.') do
+    it { should have_env 'TZ' }
+    it { should have_env 'DEBIAN_FRONTEND' }
+    it { should have_cmd ['/usr/bin/zsh', '-'] }
+    describe docker_run(described_image) do
+      describe 'dotfiles should be set up' do
+        describe file('/dotfiles') do
+          it { should exist }
+          it {should be_directory }
+        end
+        describe file('/root/.bin') do
+          it { should exist }
+          it {should be_directory }
+        end
+      end
+      describe 'vim is set up' do
+        describe command('vim --version') do
+          its(:stdout) { should match('VIM - Vi IMproved 8.1') }
+          its(:exit_status) { should eq 0 }
+        end
+        describe file('/root/.vimrc') do
+          it { should exist }
+        end
+        describe file('/root/.vim') do
+          it { should exist }
+          it {should be_directory }
+        end
+        describe file('/root/.vim/autoload/plug.vim') do
+          it { should exist }
+        end
+        describe file ('/usr/local/share/diff-highlight') do
+          it { should exist }
+        end
+        describe 'lang servers work' do
+          describe command('solargraph') do
+            its(:exit_status) { should eq 0 }
+          end
+        end
+      end
+      describe 'rust works' do
+        ['cargo', 'rustc'].each do |cmd|
+          describe command('#{cmd} --version') do
+            its (:exit_status) { should eq 0 }
+          end
+        end
+      end
+      describe 'packages were installed' do
+        ['curl', 'default-jdk', 'git', 'jq', 'nodejs', 'npm', 'php', 'python3',
+         'python3-pip', 'tmux', 'wget', 'zsh'].each do |package|
+          describe package(package) do
+            it { should be_installed }
+          end
+        end
+        ['awscli', 'beautifulsoup4', 'requests'].each do |package|
+          describe package(package) do
+            it { should be_installed.by('pip') }
+          end
+        end
+      end
+    end
+  end
+end
